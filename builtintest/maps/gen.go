cfcbd6b23f77b88410be7b272c744c0b8055cfe2
Rick <rickb777@github.com>
2017-01-07 12:38:06 +0000

    Further work on maps

--- builtintest/maps/gen.go
+++ builtintest/maps/gen.go
@@ -2,25 +2,31 @@ package maps
 
 // Code generation with non-pointer values
 
-//go:generate runtemplate -tpl maps/simple.tpl Prefix=SX Key=string Type=string Comparable=true Mutable=true
-//go:generate runtemplate -tpl maps/simple.tpl Prefix=SX Key=string Type=Apple
-//go:generate runtemplate -tpl maps/simple.tpl Prefix=SX Key=Apple  Type=string
-//go:generate runtemplate -tpl maps/simple.tpl Prefix=SX Key=Apple  Type=Pear
+//go:generate runtemplate -tpl maps/simple.tpl     Prefix=SX Key=int    Type=int    Mutable=true Comparable=true Stringer=true
+//go:generate runtemplate -tpl maps/simple.tpl     Prefix=SX Key=string Type=string Mutable=true Comparable=true
+//go:generate runtemplate -tpl maps/simple.tpl     Prefix=SX Key=string Type=Apple  Mutable=true
+//go:generate runtemplate -tpl maps/simple.tpl     Prefix=SX Key=Apple  Type=string Mutable=true
+//go:generate runtemplate -tpl maps/simple.tpl     Prefix=SX Key=Apple  Type=Pear
 
-//go:generate runtemplate -tpl maps/simple.tpl Prefix=SP Key=*string Type=*string Comparable=true Mutable=true
-//go:generate runtemplate -tpl maps/simple.tpl Prefix=SP Key=*string Type=*Apple
-//go:generate runtemplate -tpl maps/simple.tpl Prefix=SP Key=*Apple  Type=*string
-//go:generate runtemplate -tpl maps/simple.tpl Prefix=SP Key=*Apple  Type=*Pear
-
-
-//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TX Key=string Type=string Comparable=true Mutable=true
-//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TX Key=string Type=Apple
-//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TX Key=Apple  Type=string
+//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TX Key=int    Type=int    Mutable=true Comparable=true Stringer=true
+//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TX Key=string Type=string Mutable=true Comparable=true
+//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TX Key=string Type=Apple  Mutable=true
+//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TX Key=Apple  Type=string Mutable=true
 //go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TX Key=Apple  Type=Pear
 
-//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TP Key=*string Type=*string Comparable=true Mutable=true
-//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TP Key=*string Type=*Apple
-//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TP Key=*Apple  Type=*string
+
+// Code generation with pointer values
+
+//go:generate runtemplate -tpl maps/simple.tpl     Prefix=SP Key=*int    Type=*int    Mutable=true Comparable=true Stringer=true
+//go:generate runtemplate -tpl maps/simple.tpl     Prefix=SP Key=*string Type=*string Mutable=true Comparable=true
+//go:generate runtemplate -tpl maps/simple.tpl     Prefix=SP Key=*string Type=*Apple  Mutable=true
+//go:generate runtemplate -tpl maps/simple.tpl     Prefix=SP Key=*Apple  Type=*string Mutable=true
+//go:generate runtemplate -tpl maps/simple.tpl     Prefix=SP Key=*Apple  Type=*Pear
+
+//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TP Key=*int    Type=*int    Mutable=true Comparable=true Stringer=true
+//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TP Key=*string Type=*string Mutable=true Comparable=true
+//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TP Key=*string Type=*Apple  Mutable=true
+//go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TP Key=*Apple  Type=*string Mutable=true
 //go:generate runtemplate -tpl maps/threadsafe.tpl Prefix=TP Key=*Apple  Type=*Pear
 
 
