ecd66e335fdb2abbeb5e2ad1f5ce8e48f1b5236d
Rick <rick777@github.com>
2017-06-09 14:48:20 +0100

    documentation

---
+++ builtintest/doc.go
@@ -0,0 +1,2 @@
+// This package contains machine-generated Go collection examples. They provide exanple/reference source code and also form the basis for the extension unit tests.
+package builtintest
