4cedfab37ab38d91947d46a34af097c2e5deebd2
Rick <rick777@github.com>
2017-06-09 14:43:04 +0100

    documentation

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 // This package provides a template generation tool. Although this is mostly aimed at Go source code generation, it has potential for far wider usage.
 //
 // See
-//  -  Overview and usage https://github.com/rickb777/runtemplate/blob/master/README.md
-//  -  Suite of built-in templates for type-safe collections in Go https://github.com/rickb777/runtemplate/blob/master/BUILTIN.md
+// - Overview and usage https://github.com/rickb777/runtemplate/blob/master/README.md
+// - Suite of built-in templates for type-safe collections in Go https://github.com/rickb777/runtemplate/blob/master/BUILTIN.md
 package main
