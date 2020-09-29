a232f0f25edcd4ff5ff4c7b87c4011356a96e3f9
Rick <1358735+rickb777@users.noreply.github.com>
2020-09-29 22:33:52 +0100

    ended maintenance for v1 branch; v3 fully supersedes it now.

--- builtintest/doc.go
+++
@@ -1,2 +0,0 @@
-// This package contains machine-generated Go collection examples. They provide example/reference source code and also form the basis for the extension unit tests.
-package builtintest
--- version.go
+++
@@ -1,4 +0,0 @@
-package main
-
-// appVersion is updated manually just prior to the next tag to be used.
-const appVersion = "v3.4.2"
